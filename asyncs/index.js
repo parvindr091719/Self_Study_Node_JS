7fec196063e6b2d14c91360d0a93e2ce6570f4e5
parvindr091719 <parinvdr091719@gmail.com>
2022-09-04 23:19:03 +0530

    All Async Data Move (asyncs folder) with html

---
+++ asyncs/index.js
@@ -0,0 +1,13 @@
+// const fs = require("fs");
+
+// const data = fs.readFileSync("async.txt", "utf-8");
+// console.log(data);
+
+
+//A-Sync 
+// fs.readFile("async.txt", "utf-8", (err, datas)=>{
+//     console.log(datas);
+// })
+// 
+// console.log("After the Sync");
+// console.log("After the Sync");

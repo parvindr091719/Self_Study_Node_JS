bf5624c782a8e10f597f3e3a0c9e3fed7f59273a
parvindr091719 <parinvdr091719@gmail.com>
2022-09-04 22:09:37 +0530

    OsModule 04092022

---
+++ OsModule/osmod.js
@@ -0,0 +1,25 @@
+
+// bit check
+const os = require("os");
+// console.log(os.arch());
+
+// freeMemory check
+// const freeMemory = os.freemem();
+// // console.log(freeMemory);
+// console.log(`${freeMemory / 1024 / 1024 / 1024}`);
+// freeMemory check
+
+
+// check total Memory
+// const totalMemory = os.totalmem();
+// console.log(`${totalMemory / 1024 / 1024 / 1024}`);
+
+// console.log(os.arch());
+// console.log(os.hostname());
+// console.log(os.platform());
+
+console.log(os.tmpdir());
+console.log(os.type());
+
+
+
